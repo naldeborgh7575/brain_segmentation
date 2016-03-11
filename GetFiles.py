a0a607a658db4af33894664504accdbb8ff3f368
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-03-10 22:37:09 -0700

    add option to get ground truth files

--- GetFiles.py
+++ GetFiles.py
@@ -20,10 +20,19 @@ class GetFiles(object):
             return self._get_t1c_()
         elif self.sequence == 'flair':
             return self._get_flair_()
+        elif self.sequence == 'gt':
+            return self._get_gt_()
         elif self.sequence == 'all':
             return self._get_all_()
         else:
             return 'please initialize with a valid sequence or "all"'
+
+    def _get_gt_(self):
+        gr = self.grade
+        dir = 'Training/'
+        if self.grade == 'both':
+            gr = '**'
+        return glob(dir + gr + '/**/VSD.Brain_*/*.mha')
 
     def _get_t2_(self):
         gr = self.grade
