dae8ffa3248473d91afcbf1473f1d14fa9ad3978
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-03-30 19:04:35 -0600

    add old tag to n4bias program interwebs

---
+++ N4BiasFieldCorrection_old.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+
+import SimpleITK as sitk
+import sys
+import os
+
+if len ( sys.argv ) < 2:
+    print( "Usage: N4BiasFieldCorrection inputImage " + "outputImage [shrinkFactor] [maskImage] [numberOfIterations] " + "[numberOfFittingLevels]" )
+    sys.exit ( 1 )
+
+inputImage = sitk.ReadImage( sys.argv[1] )
+
+if len ( sys.argv ) > 4:
+    maskImage = sitk.ReadImage( sys.argv[4] )
+else:
+    maskImage = sitk.OtsuThreshold( inputImage, 0, 1, 200 )
+
+if len ( sys.argv ) > 3:
+    inputImage = sitk.Shrink( inputImage, [ int(sys.argv[3]) ] * inputImage.GetDimension() )
+    maskImage = sitk.Shrink( maskImage, [ int(sys.argv[3]) ] * inputImage.GetDimension() )
+
+inputImage = sitk.Cast( inputImage, sitk.sitkFloat32 )
+corrector = sitk.N4BiasFieldCorrectionImageFilter();
+numberFilltingLevels = 4
+
+if len ( sys.argv ) > 6:
+    numberFilltingLevels = int( sys.argv[6] )
+
+if len ( sys.argv ) > 5:
+    corrector.SetMaximumNumberOfIterations( [ int( sys.argv[5] ) ] *numberFilltingLevels  )
+
+output = corrector.Execute( inputImage, maskImage )
+
+sitk.WriteImage( output, sys.argv[2] )
+
+if ( not "SITK_NOSHOW" in os.environ ):
+    sitk.Show( output, "N4 Corrected" )
