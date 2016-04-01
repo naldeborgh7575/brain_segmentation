9bc487a13ac9fb33b7b6ab893f02a1bb791d0298
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-04-01 11:14:51 -0600

    add prgressbar to clean up

--- write_to_s3.py
+++ write_to_s3.py
@@ -1,8 +1,11 @@
-import sys
+# import sys
+import time
 import os
+import progressbar
 import threading
 from glob import glob
 from boto.s3.connection import S3Connection
+progress = progressbar.ProgressBar(widgets=[progressbar.Bar('*', '[', ']'), progressbar.Percentage(), ' '])
 
 def files_to_s3(files, bucket_name):
     '''
@@ -13,14 +16,14 @@ def files_to_s3(files, bucket_name):
     AWS_KEY = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET = os.environ['AWS_SECRET_ACCESS_KEY']
 
-    def percent_cb(complete, total):
-        sys.stdout.write('.')
-        sys.stdout.flush()
+    # def percent_cb(complete, total):
+    #     sys.stdout.write('.')
+    #     sys.stdout.flush()
 
     def upload(myfile):
         conn = S3Connection(aws_access_key_id = AWS_KEY, aws_secret_access_key = AWS_SECRET)
         bucket = conn.get_bucket(bucket_name)
-        key = bucket.new_key(myfile).set_contents_from_filename(myfile, cb=percent_cb, num_cb=1)
+        key = bucket.new_key(myfile).set_contents_from_filename(myfile) # , cb=percent_cb, num_cb=1)
         return myfile
 
     for fname in files:
@@ -28,4 +31,11 @@ def files_to_s3(files, bucket_name):
 
 if __name__ == '__main__':
     files = glob('n4_PNG/**')
-    files_to_s3(files, 'n4itk-slices')
+    progress.currval = 0
+    start_time = time.time()
+    for x in progress(xrange(len(files) / 100)): #avoid threading complications
+        time.sleep(2)
+        f = files[100 * x : (100 * x) + 100]
+        files_to_s3(f, 'n4itk-slices')
+        # print(str(x) + ' out of ' + str(len(files)/100))
+    print('------%s seconds------' % (time.time() - start_time))
