4ffa486e3bc6f5a999fa28e925dec18d60aad2e0
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-03-31 20:11:01 -0600

    added function to save n4itk images in same format as others

--- brain_pipeline.py
+++ brain_pipeline.py
@@ -24,7 +24,6 @@ class BrainPipeline(object):
         # [ [slice1 x 5], [slice2 x 5], ..., [slice155 x 5]]
         self.slices_by_slice = n
         self.normed_slices = self.norm_slices()
-        self.n4_normed_slices = None
 
     def read_scans(self):
         '''
@@ -34,18 +33,21 @@ class BrainPipeline(object):
         print 'Loading scans...'
         slices_by_mode = np.zeros((5, 155, 240, 240))
         slices_by_slice = np.zeros((155, 5, 240, 240))
-        scans = glob(self.path + '/**/*.mha') # directories to each image (5 total)
+        flair = glob(self.path + '/*Flair*/*.mha')
+        t2 = glob(self.path + '/*_T2*/*.mha')
+        gt = glob(self.path + '/*more*/*.mha')
+        t1s = glob(self.path + '/**/*T1*.mha')
+        t1_n4 = glob(self.path + '/*T1*/*_n.mha')
+        t1 = [scan for scan in t1s if scan not in t1_n4]
+        scans = [flair[0], t1s[0], t1[1], t2[0], gt[0]] # directories to each image (5 total)
         if self.n4itk_apply:
             print '-> Applyling bias correction...'
-            t1_scans = glob(self.path + '/**/*T1*.mha') # t1 scans to norm
-            for t1_path in t1_scans:
+            for t1_path in t1:
                 self.n4itk_norm(t1_path) # normalize files
-                n4_scans = glob(self.path + '/*T1*/*_n.mha')
-            scans[1:3] = n4_scans # replace t1 with normed t1
+                scans = [flair[0], t1_n4[0], t1_n4[1], t2[0], gt[0]]
         elif self.n4itk:
-            n4_scans = glob(self.path + '/*T1*/*n_.mha')
-            scans[1:3] = n4_scans # replace t1 with normed t1
-        for scan_idx in xrange(4):
+            scans = [flair[0], t1_n4[0], t1_n4[1], t2[0], gt[0]]
+        for scan_idx in xrange(5):
             # import pdb; pdb.set_trace()
             # read each image directory, save to self.slices
             slices_by_mode[scan_idx] = io.imread(scans[scan_idx], plugin='simpleitk').astype(float)
@@ -101,14 +103,14 @@ class BrainPipeline(object):
             self.patches.append(zip(patch_list[0], patch_list[1], patch_list[2], patch_list[3]))
             self.patch_labels.append(patch_list[-1])
 
-    def save_patient(self, reg_or_norm, patient_num):
+    def save_patient(self, reg_norm_n4, patient_num):
         '''
         INPUT:  (1) int 'patient_num': unique identifier for each patient
-                (2) string 'reg_or_norm': 'reg' for original images, 'norm' normalized images
+                (2) string 'reg_or_norm': 'reg' for original images, 'norm' normalized images, 'n4' for n4 normalized images
         OUTPUT: saves png in Norm_PNG directory for normed, Training_PNG for reg
         '''
         print 'Saving scans for patient {}...'.format(patient_num)
-        if reg_or_norm == 'norm': #saved normed slices
+        if reg_norm_n4 == 'norm': #saved normed slices
             for slice_ix in xrange(155): # reshape to strip
                 strip = self.normed_slices[slice_ix].reshape(1200, 240)
                 if np.max(strip) != 0: # set values < 1
@@ -117,12 +119,21 @@ class BrainPipeline(object):
                     strip /= abs(np.min(strip))
                 # save as patient_slice.png
                 io.imsave('Norm_PNG/{}_{}.png'.format(patient_num, slice_ix), strip)
-        else:
+        elif reg_norm_n4 == 'reg':
             for slice_ix in xrange(155):
                 strip = self.slices_by_slice[slice_ix].reshape(1200, 240)
                 if np.max(strip) != 0:
                     strip /= np.max(strip)
                 io.imsave('Training_PNG/{}_{}.png'.format(patient_num, slice_ix), strip)
+        else:
+            for slice_ix in xrange(155): # reshape to strip
+                strip = self.normed_slices[slice_ix].reshape(1200, 240)
+                if np.max(strip) != 0: # set values < 1
+                    strip /= np.max(strip)
+                if np.min(strip) <= -1: # set values > -1
+                    strip /= abs(np.min(strip))
+                # save as patient_slice.png
+                io.imsave('n4_PNG/{}_{}.png'.format(patient_num, slice_ix), strip)
 
     def n4itk_norm(self, path, n_dims=3, n_iters='[20,20,10,5]'):
         '''
@@ -134,17 +145,23 @@ class BrainPipeline(object):
         # run n4_bias_correction.py path n_dim n_iters output_fn
         subprocess.call('python n4_bias_correction.py ' + path + ' ' + str(n_dims) + ' ' + n_iters + ' ' + output_fn, shell = True)
 
+
+def save_patient_slices(patients, type):
+    '''
+    INPUT   (1) list 'patients': paths to any directories of patients to save. for example- glob("Training/HGG/**")
+            (2) string 'type': options = reg (non-normalized), norm (normalized, but no bias correction), n4 (bias corrected and normalized)
+    saves strips of patient slices to approriate directory (Training_PNG/, Norm_PNG/ or n4_PNG/) as patient-num_slice-num
+    '''
+    for patient_num, path in enumerate(patients):
+        a = BrainPipeline(path)
+        a.save_patient(type, patient_num)
+
+
 if __name__ == '__main__':
     patients = glob('Training/HGG/**')
-    for patient in patients[13:50]:
-        a = BrainPipeline(patient, n4itk_apply = True)
-
-    # a = BrainPipeline(patients[0])
-    # for patient_num, path in enumerate(patients):
-    #     a = BrainPipeline(path)
-        # a.save_patient('norm', patient_num)
-        # a.save_patient('reg', patient_num)
-
+    # save_patient_slices(patients, 'reg')
+    # save_patient_slices(patients, 'norm')
+    save_patient_slices(patients, 'n4')
 
     # TO DO
     # make generator to train net in batches (~32)
