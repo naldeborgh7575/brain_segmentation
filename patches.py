b0c0a4beacfcef1ba66a34a32cf1c3291af6c5b0
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-04-01 18:46:36 -0600

    add ability to random patches in find_patches

--- patches.py
+++ patches.py
@@ -13,54 +13,71 @@ def find_patches(training_images, class_num, num_samples, patch_size=(65,65)):
     INPUT:  (1) list 'training_images': all training images to select from
             (2) int 'class_num': class to sample from choice of {0, 1, 2, 3, 4}.
             (3) tuple 'patch_size': dimensions of patches to be generated defaults to 65 x 65
-    OUTPUT: (1) num_samples patches from class 'class_num' randomly selected
+    OUTPUT: (1) num_samples patches from class 'class_num' randomly selected. note- if class_num is 0, will choose patches randomly, not exclusively 0s.
     '''
     ct = 0
-    patches = [] #list of all patches
+    patches, l = [], [] #list of all patches and associated labels
+    if class_num == 0:
+        while ct < num_samples:
+            im_path = random.choice(training_images)
+            patch, label = random_patches(im_path, patch_size = patch_size)
+            patches.append(patch)
+            l.append(label)
+            ct += 1
+        return patches,l
+
     h,w = patch_size[0], patch_size[1]
     while ct < num_samples:
         im_path = random.choice(training_images) # select image to sample from
         fn = os.path.basename(im_path)
         label = io.imread('Labels/' + fn[:-4] + 'L.png')
-
         if class_num not in np.unique(label): # no pixel label class_num in img
             continue
-
-        img = io.imread(im_path).reshape(5, 240, 240)[:-1]
+        img = io.imread(im_path).reshape(5, 240, 240)[:-1] # exclude label slice
         p = random.choice(np.argwhere(label == class_num)) # center pixel
         while p[0] < h/2 or p[1] < h/2: # if patch won't fit
             p = random.choice(np.argwhere(label == class_num))
 
         p_ix = (p[0]-(h/2), p[0]+((h+1)/2), p[1]-(w/2), p[1]+((w+1)/2)) # patch index
         patch = np.array([i[p_ix[0]:p_ix[1], p_ix[2]:p_ix[3]] for i in img])
+        l.append(label[p_ix[0]:p_ix[1], p_ix[2]:p_ix[3]])
         patches.append(patch) # patch = (n_chan, h, w)
-        import pdb; pdb.set_trace()
         ct += 1
-    return np.array(patches)
+    return patches, l
+
+def random_patches(im_path, patch_size = (65,65)):
+    fn = os.path.basename(im_path)
+    patch_lst = []
+    label = io.imread('Labels/' + fn[:-4] + 'L.png')
+    imgs = (io.imread(im_path).reshape(5, 240, 240))
+    imgs[4] = label
+    for img in imgs:
+        patch_lst.append(extract_patches_2d(img, patch_size, max_patches = 1, random_state=5)) #set rs for same patch ix among modes
+    patches = np.array(zip(patch_lst[0], patch_lst[1], patch_lst[2], patch_lst[3])[0])
+    patch_label = np.array(patch_lst[-1][0])
+    return patches, patch_label
 
 
-
-
-def generate_patches(img_path, patch_size=(65,65), num_patches = 10):
-    '''
-    Generates patches (num_chan, patch_h, patch_w) for an input image
-    INPUT:  (1) string 'img_path': path to imput image (png, strip of slices)
-            (2) tuple 'patch_size': dimensions of patches to be used in net
-            (3) int 'num_patches': number of patches to be generated per slice.
-    OUTPUT: (1) list of scan patches: (num_slices * num_patches, num_channels, patch_h, patch_w)
-            (2) list of label patches: (num_slices * num_patches, patch_h, patch_w)
-    '''
-    patch_lst = [] # list of lists: patches for each slice (same idxs)
-    patch_labels = []
-    slices = io.imread(img_path).reshape(5,240,240) # (chan + gt, h, w)
-    for img in slices:
-        patch_lst.append(extract_patches_2d(img, patch_size, max_patches = num_patches, random_state=5)) #set rs for same patch ix among modes
-    patches = np.array(zip(patch_lst[0], patch_lst[1], patch_lst[2], patch_lst[3]))
-    patch_labels = np.array([i[patch_size[0] / 2][patch_size[1]/2] for i in patch_lst[-1]])
-    return patches, patch_labels
-
-def balance_classes_bin(patches, labels):
-    pass
+# def generate_patches(img_path, patch_size=(65,65), num_patches = 10):
+#     '''
+#     Generates patches (num_chan, patch_h, patch_w) for an input image
+#     INPUT:  (1) string 'img_path': path to imput image (png, strip of slices)
+#             (2) tuple 'patch_size': dimensions of patches to be used in net
+#             (3) int 'num_patches': number of patches to be generated per slice.
+#     OUTPUT: (1) list of scan patches: (num_slices * num_patches, num_channels, patch_h, patch_w)
+#             (2) list of label patches: (num_slices * num_patches, patch_h, patch_w)
+#     '''
+#     patch_lst = [] # list of lists: patches for each slice (same idxs)
+#     patch_labels = []
+#     fn = os.path.basename(im_path)
+#     label = io.imread('Labels/' + fn[:-4] + 'L.png')
+#     slices = io.imread(img_path).reshape(5,240,240)[:-1] # (chan + gt, h, w)
+#     slices = slices.append(label)
+#     for img in slices:
+#         patch_lst.append(extract_patches_2d(img, patch_size, max_patches = num_patches, random_state=5)) #set rs for same patch ix among modes
+#     patches = np.array(zip(patch_lst[0], patch_lst[1], patch_lst[2], patch_lst[3]))
+#     patch_labels = np.array([i[patch_size[0] / 2][patch_size[1]/2] for i in patch_lst[-1]])
+#     return patches, patch_labels
 
     # for slice_strip in self.normed_slices: # slice = strip of 5 images
     #     slices = slice_strip.reshape(5,240,240)
