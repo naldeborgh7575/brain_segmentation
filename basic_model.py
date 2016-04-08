c3b3ab4bd8b861d4dec3861a1e7b49f266ff9055
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-04-07 19:47:54 -0600

    increase batch size, add one conv layer at the end

--- basic_model.py
+++ basic_model.py
@@ -11,9 +11,10 @@ from keras.constraints import maxnorm
 from keras.utils import np_utils
 
 class BasicModel(object):
-    def __init__(self, single_or_cascade='single', n_epoch=10):
+    def __init__(self, single_or_cascade='single', n_epoch=10, batch_size=128):
         self.single_or_cascade = single_or_cascade
         self.n_epoch = n_epoch
+        self.batch_size = batch_size
         self.model_comp = self.compile_model()
 
     def compile_model(self):
@@ -23,27 +24,25 @@ class BasicModel(object):
         single.add(Activation('relu'))
         single.add(Dropout(0.5))
         single.add(Convolution2D(nb_filter=128, nb_row=7, nb_col=7, activation='relu', border_mode='valid', W_regularizer=l1l2(l1=0.01, l2=0.01)))
-        single.add(Dropout(0.5))
         single.add(MaxPooling2D(pool_size=(4,4), strides=(1,1)))
         single.add(Dropout(0.5))
         single.add(Convolution2D(nb_filter=128, nb_row=5, nb_col=5, activation='relu', border_mode='valid', W_regularizer=l1l2(l1=0.01, l2=0.01)))
         single.add(MaxPooling2D(pool_size=(2,2), strides=(1,1)))
+        single.add(Convolution2D(nb_filter=128, nb_row=3, nb_col=3, activation='relu', border_mode='valid', W_regularizer=l1l2(l1=0.01, l2=0.01)))
         single.add(Dropout(0.5))
 
         single.add(Flatten())
-        single.add(Dense(256))
-        single.add(Dropout(0.5))
         single.add(Dense(5))
         single.add(Activation('softmax'))
 
-        sgd = SGD(lr=0.01, decay=0.1)
+        sgd = SGD(lr=0.0001, decay=0.1)
         single.compile(loss='categorical_crossentropy', optimizer='sgd')
         print 'Done.'
         return single
 
     def fit_model(self, X_train, y_train):
         Y_train = np_utils.to_categorical(y_train, 5)
-        self.model_comp.fit(X_train, Y_train, batch_size=32, nb_epoch=self.n_epoch, validation_split=0.1, show_accuracy=True, verbose=1)
+        self.model_comp.fit(X_train, Y_train, batch_size=self.batch_size, nb_epoch=self.n_epoch, validation_split=0.1, show_accuracy=True, verbose=1)
 
 
 
